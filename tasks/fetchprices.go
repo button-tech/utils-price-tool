9757d5790d5766a580d4cd26f5b8f4ff08a652c4
jeyldii <re3fo@mail.ru>
2019-09-08 19:13:34 +0300

    ref

--- tasks/fetchprices.go
+++ tasks/fetchprices.go
@@ -10,44 +10,6 @@ import (
 	"time"
 )
 
-//func NewGetGroupTask(timeout time.Duration, s services.Service, store storage.Storage) {
-//	ticker := time.Tick(timeout)
-//	wg := sync.WaitGroup{}
-//
-//	go func() {
-//		for _ = range ticker {
-//			tokens := services.InitRequestData()
-//
-//			ch := make(chan storage.GotPrices, 4)
-//			var stored []storage.GotPrices
-//
-//			for _, t := range tokens.Tokens {
-//				wg.Add(1)
-//
-//				go func(wg *sync.WaitGroup, t *services.TokensWithCurrency, ch chan storage.GotPrices) {
-//					defer wg.Done()
-//
-//					got, err := s.GetPricesCMC(t)
-//					if err != nil {
-//						log.Println(err)
-//						return
-//					}
-//
-//					ch <- got
-//
-//				}(&wg, &t, ch)
-//				item := <- ch
-//				stored = append(stored, item)
-//			}
-//			wg.Wait()
-//			store.Update(&stored)
-//
-//		}
-//	}()
-//
-//	store.Get()
-//}
-
 type DuiCont struct {
 	TimeOut time.Duration
 	Service services.Service
