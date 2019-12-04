faa11be9ec0db14189600712e4d52db7305a34f7
jeyldii <re3fo@mail.ru>
2019-12-05 02:05:55 +0300

    Refactored main package + go.mod

--- cmd/pricetool/main.go
+++ cmd/pricetool/main.go
@@ -1,35 +1,28 @@
 package main
 
 import (
-	"github.com/button-tech/logger"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
+	"github.com/button-tech/logger"
 	"github.com/button-tech/utils-price-tool/api"
+	"github.com/button-tech/utils-price-tool/pkg/storage"
 	"github.com/button-tech/utils-price-tool/services"
-	"github.com/button-tech/utils-price-tool/storage"
 	"github.com/button-tech/utils-price-tool/tasks"
-	"github.com/valyala/fasthttp"
 )
 
 func main() {
 	store := storage.NewCache()
-	go tasks.NewGetGroup(services.New(store), store)
+	getPrices := services.New(store)
+	go tasks.FetchGroup(getPrices, store)
 
 	if err := logger.InitLogger(os.Getenv("DSN")); err != nil {
 		log.Fatal(err)
 	}
 
 	s := api.NewServer(store)
-	server := fasthttp.Server{
-		Handler:      s.R.HandleRequest,
-		ReadTimeout:  time.Second * 30,
-		WriteTimeout: time.Second * 30,
-	}
-
 	signalEx := make(chan os.Signal, 1)
 	defer close(signalEx)
 
@@ -40,12 +33,12 @@ func main() {
 		syscall.SIGQUIT)
 
 	go func() {
-		if err := server.ListenAndServe(":5000"); err != nil {
+		if err := s.Core.ListenAndServe(":5000"); err != nil {
 			logger.Fatal(err)
 		}
 	}()
 	defer func() {
-		if err := server.Shutdown(); err != nil {
+		if err := s.Core.Shutdown(); err != nil {
 			logger.Fatal(err)
 		}
 	}()
